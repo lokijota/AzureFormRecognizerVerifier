b7549483649402eaf771d3a5b553c4d8a50df832
João Pedro Martins <joao.pedro.martins@gmail.com>
2019-09-02 21:34:25 +0200

    Cleaned up project settings and using statements

--- FormRecognizerVerifier/Program.cs
+++ FormRecognizerVerifier/Program.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using ImageMagick;
-using System.Collections.Generic;
-using System.Linq;
-using Newtonsoft.Json;
+﻿using ImageMagick;
 using Newtonsoft.Json.Linq;
+using System;
 using System.IO;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.ComTypes;
 
 namespace FormRecognizerVerifier
 {
